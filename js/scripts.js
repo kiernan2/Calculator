16d73755c5ece9708a512b92f8874126626f7dff
Kiernan2 <kiernan1994@gmail.com>
2021-06-16 14:55:28 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+function saySomething(whatToSay) {
+  alert(whatToSay);
+}
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2){
+	return number1 - number2;
+}
+
+function multiply(number1, number2){
+	return number1 * number2;
+}
+
+function threeTimes(number1, number2, number3){
+	return number1 * number2 * number3;
+}
+
+function divide(number1, number2){
+	return number1 / number2;
+}
+
+function remainder(number1, number2){
+	return number1 % number2;
+}
+
+const number1 = parseInt(prompt("Enter a number"));
+
+const number2 = parseInt(prompt("Enter another number"));
+
+const result add(number1, number2)
+
+alert(result);
